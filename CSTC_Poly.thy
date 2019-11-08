41349573702ca42c20ede200aa3aa4647d92897e
riedersa <sabine.rieder@tum.de>
2019-11-08 14:02:43 +0100

    continued with proof for polynomial time of CNF-SAT to Clique

--- CSTC_Poly.thy
+++ CSTC_Poly.thy
@@ -8,7 +8,7 @@ definition "max_size_clauses xs = card (\<Union> (set xs))"
 definition "add_edges_cstc F S = 
   SPECT [ S \<union> {{(l1, i), (l2, j)} | l1 l2 i j.
       i < length F \<and> j < length F \<and> l1 \<in> F ! i \<and> l2 \<in> F ! j \<and> \<not> conflict l1 l2 \<and> i \<noteq> j}
-       \<mapsto> max_size_clauses F * length F * length F]"
+       \<mapsto> max_size_clauses F * max_size_clauses F * length F * length F]"
 
 definition "add_nodes_cstc F V =
   SPECT [ V \<union>  {(l1, i) | l1 i. i < length F \<and>   l1 \<in> F ! i}
@@ -34,7 +34,7 @@ definition "size_CNF_SAT xs = number_clauses_CNF_SAT xs * max_size_clauses xs"
 (*with number of clauses time  maximum length of the clauses n*)
 definition "cnf_sat_to_clique_time n = 4 + n * n  + n"
 definition "size_Clique = (\<lambda>(E,V,k). card E + card V)"
-definition "cnf_sat_to_clique_space n  = n +n * n"
+definition "cnf_sat_to_clique_space n  = n * n + n"
 
 lemma upperbounding_card_m: "\<forall>x\<in>X. card x \<le> m \<and> finite x \<Longrightarrow> x\<in>X \<Longrightarrow> y\<in>X \<Longrightarrow> 
         card {{f l1, g l2} |l1 l2. l1 \<in> x \<and> l2 \<in> y \<and> h l1 l2} \<le> m*m"
@@ -70,44 +70,53 @@ qed
 lemma card_clauses_samller: "x \<in> set xs \<and> finite (\<Union> (set xs)) \<Longrightarrow> card x \<le> max_size_clauses xs"
   by(auto simp add: max_size_clauses_def aux0)
 
+lemma card_V:
+  assumes "finite (\<Union> (set p))"
+  shows "card {(l1, i). i < length p \<and> l1 \<in> p ! i} \<le> length p * card (\<Union> (set p))"
+proof -
+  let ?S = "((\<Union> (set p)) \<times> {0..<length p})"
+  let ?V = " {(l1, i). i < length p \<and> l1 \<in> p ! i}"
+  have 1: "card ?S \<le> (card (\<Union> (set p))) * length p" 
+    by (simp add: card_cartesian_product)
+  have "?V \<subseteq> ?S" by(auto)+ 
+  then show ?thesis using 1 
+    by (metis (no_types, lifting) assms card_mono dual_order.trans finite_SigmaI finite_atLeastLessThan mult.commute) 
+qed
 
-lemma cnf_sat_to_clique_size: "size_Clique (cnf_sat_to_clique p) \<le> cnf_sat_to_clique_space (size_CNF_SAT xs)" 
-  (*apply(auto simp: size_Clique_def cnf_sat_to_clique_def cnf_sat_to_clique_space_def size_CNF_SAT_def number_clauses_CNF_SAT_def max_size_clauses_def card_clauses_samller)
-  using card_clauses_samller apply(rule order.trans[OF card_Un_le])
+lemma aux2: "finite E' \<and> E \<subseteq> E' \<Longrightarrow> card E \<le> card E'"
+  by (simp add: card_mono)
+
+lemma card_E:
+  assumes "finite (\<Union> (set F))"
+  shows "card {{(l1, i), (l2, j)} |l1 l2 i j. i < length F \<and> j < length F \<and> i \<noteq> j \<and> \<not> conflict l1 l2 \<and> l1 \<in> F ! i \<and> l2 \<in> F ! j}
+            \<le> length F * card (\<Union> (set F)) * (length F * card (\<Union> (set F)))"
+proof -
+  have 0: "\<forall>x\<in> set F. finite x" using assms 
+    by (metis Set.set_insert Un_infinite Union_insert)  
+  let ?S = "((\<Union> (set F)) \<times> {0..<length F}) \<times> ((\<Union> (set F)) \<times> {0..<length F})"
+  let ?E = "{{(l1, i), (l2, j)} |l1 l2 i j. i < length F \<and> j < length F \<and> i \<noteq> j \<and> \<not> conflict l1 l2 \<and> l1 \<in> F ! i \<and> l2 \<in> F ! j}"
+  let ?E' = "{{(l1, i), (l2, j)} |l1 l2 i j. i < length F \<and> j < length F \<and> l1 \<in> F ! i \<and> l2 \<in> F ! j}"
+  have 1: "?E \<subseteq>?E'" by(auto)
+  have fin_E: "finite ?E"
+    using assms 
+    using assms by (fastforce intro: finite_surj[where A = "?S"])
+  have fin_E': "finite ?E'"
+    using assms 
+      using assms by (fastforce intro: finite_surj[where A = "?S"])
+  then have 2: "card ?E \<le> card ?E'" 
+    by (simp add: CSTC_Poly.aux2 1) 
+  have "card ?E' \<le> 
+        length F * card (\<Union> (set F)) * (length F * card (\<Union> (set F)))" using assms card_clauses_samller fin_E'  sorry
+  then show ?thesis using 1 2 by(auto) 
+qed
+
+
+lemma cnf_sat_to_clique_size: "size_Clique (cnf_sat_to_clique p) \<le> cnf_sat_to_clique_space (size_CNF_SAT p)" 
+  apply(auto simp: size_Clique_def cnf_sat_to_clique_def cnf_sat_to_clique_space_def size_CNF_SAT_def number_clauses_CNF_SAT_def max_size_clauses_def card_clauses_samller)
   apply(rule add_mono)
-  subgoal
-    apply(subst paf)
-    apply(rule order.trans) apply(rule card_Un) apply simp
-    apply(rule order.trans)
-     apply(rule sum_image_le) apply simp
-     apply simp
-    apply(rule order.trans) apply(rule sum_mono[where g="(\<lambda>_. m*m)"] )
-    subgoal for i apply simp
-      apply(rule upperbounding_card3) by auto
-    apply simp
-    done
-
-  subgoal
-    apply(subst paf2)
-    apply(subst paf)
-    apply(rule order.trans) apply(rule card_Un) apply simp
-    apply(rule order.trans)
-     apply(rule sum_image_le) apply simp apply simp
-    apply(rule order.trans)
-     apply(rule sum_mono[where g="\<lambda>_. 9 * length p"])
-    subgoal apply simp
-    apply(rule order.trans) apply(rule card_Un) apply simp
-    apply(rule order.trans)
-       apply(rule sum_image_le) apply simp apply simp
-      apply(rule order.trans)
-       apply(rule sum_mono[where g="\<lambda>_. 9"])
-      subgoal
-        apply simp apply(rule upperbounding_card3) by auto  
-      apply simp done  
-    subgoal apply simp done
-    done
-  done*)
-  sorry
+   apply(auto simp add: card_E)
+  apply(auto simp add: card_V)
+  done
 
 
 lemma cnf_sat_to_clique_refines:
