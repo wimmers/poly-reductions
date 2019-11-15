24d5c100a5a4ecd2522030e8286ab2db79cc89e7
riedersa <sabine.rieder@tum.de>
2019-11-15 14:39:00 +0100

    some work for fun

---
+++ VC_To_FNS.thy
@@ -0,0 +1,17 @@
+theory VC_To_FNS
+  imports Main "Three_Sat_To_Set_Cover" Graph_Theory.Graph_Theory
+begin
+
+fun del_verts:: "('a, 'b) pre_digraph \<Rightarrow> 'a list \<Rightarrow>('a,'b) pre_digraph" where
+"del_verts G [] = G" |
+"del_verts G (v#vs) = del_verts (pre_digraph.del_vert G v) vs"
+
+definition
+  "is_fns (G::('a,'b) pre_digraph) (R:: 'a list)  \<equiv> \<not>(\<exists>p. pre_digraph.cycle (del_verts G R) p) "
+
+definition
+ "fns \<equiv> {(G, k). \<exists>R. wf_digraph G \<and> length R \<le> k \<and> is_fns G R}"
+
+definition "
+  vc_to_fns VC aux = (case VC of (E, k) \<Rightarrow> (\<lparr> verts = {v. v \<in> \<Union> E}, arcs = {(u, v)| u v. {u, v} \<in> E}, tail = fst, head = snd \<rparr>, k))"
+end
