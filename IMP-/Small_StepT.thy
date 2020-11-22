2b036c19f6f0840e3a81eca54901e42d3e8a51f5
Bilel Ghorbel <ge36toh@mytum.de>
2020-11-22 03:56:43 +0100

    implement relation power instead of HOL compow

--- IMP-/Small_StepT.thy
+++ IMP-/Small_StepT.thy
@@ -5,9 +5,7 @@
 section "Small step semantics of IMP- "
 
 subsection "Small step semantics definition"
-theory Small_StepT
-  imports Main Big_StepT 
-begin
+theory Small_StepT  imports Main Big_StepT Rel_pow begin
 
 inductive
   small_step :: "com * state \<Rightarrow> com * state \<Rightarrow> bool" (infix "\<rightarrow>" 55)
@@ -27,19 +25,17 @@ WhileFalse:   "s b = 0 \<Longrightarrow> (WHILE b\<noteq>0 DO c,s) \<rightarrow>
 
 subsection "Transitive Closure"
 abbreviation
-  small_step_trans :: "com * state \<Rightarrow> nat \<Rightarrow> com * state \<Rightarrow> bool" ("_ \<rightarrow>* _ \<down> _" 55)
-  where "x \<rightarrow>* t \<down> y == (small_step ^^ t) x y"
+  small_step_pow :: "com * state \<Rightarrow> nat \<Rightarrow> com * state \<Rightarrow> bool" ("_ \<rightarrow>* _ \<down> _" 55)
+  where "x \<rightarrow>* t \<down> y == (rel_pow  small_step t)  x y"
 
 
 subsection\<open>Executability\<close>
 
 code_pred small_step .
-(* code_pred small_step_trans .
 
 values "{(c',map t [''x'',''y'',''z'']) |c' t .
    ((''x'' ::= A (V ''z'');; ''y'' ::=A ( V ''x''),
-    <''x'' := 3, ''y'' := 7, ''z'' := 5>) \<rightarrow>** (c',t))}"
-*)
+    <''x'' := 3, ''y'' := 7, ''z'' := 5>) \<rightarrow>* 1 \<down> (c',t))}"
 subsection\<open>Proof infrastructure\<close>
 
 subsubsection\<open>Induction rules\<close>
@@ -55,6 +51,7 @@ lemmas small_step_induct = small_step.induct[split_format(complete)]
 subsubsection\<open>Proof automation\<close>
 
 declare small_step.intros[simp,intro]
+
 
 text\<open>Rule inversion:\<close>
 
@@ -76,17 +73,17 @@ apply(induction arbitrary: cs'' rule: small_step.induct)
 done
 
 section "Equivalence with big-step semantics"
-
+declare rel_pow_intros[simp,intro]
 text "sequence property"
 lemma star_seq2: "(c1,s) \<rightarrow>* t \<down> (c1',s') \<Longrightarrow> (c1;;c2,s) \<rightarrow>* t  \<down> (c1';;c2,s')"
 proof(induction t arbitrary: c1 c1' s s')
   case 0
-  then show ?case by simp
+  then show ?case by auto
 next
   case (Suc t)
-  then obtain c1'' s'' where "(c1,s) \<rightarrow>* t \<down> (c1'', s'')" 
-                         and "(c1'', s'') \<rightarrow> (c1', s')" by auto
-  moreover then have "(c1;;c2, s) \<rightarrow>* t \<down> (c1'';;c2, s'')" using Suc by auto
+  then obtain c1'' s'' where "(c1,s) \<rightarrow> (c1'', s'')" 
+                         and "(c1'', s'')  \<rightarrow>* t \<down>  (c1', s')" by auto
+  moreover then have "(c1'';;c2, s'') \<rightarrow>* t \<down> (c1';;c2, s')" using Suc by simp
   ultimately show ?case by auto
 qed
 
@@ -97,18 +94,15 @@ lemma seq_comp:
 proof (induction t1 arbitrary: c1 s1)
   case 0
   then have "(c1;;c2, s1) \<rightarrow> (c2, s2)" by auto
-  then show ?case using 0 by (metis Suc_eq_plus1 add_cancel_right_left relpowp_Suc_I2)
+  then show ?case using 0  by simp
 next
   case (Suc t1)
   then obtain c1' s1' where *: "(c1, s1) \<rightarrow> (c1',s1')" and "(c1',s1') \<rightarrow>* t1 \<down> (SKIP,s2)"
-    using relpowp_Suc_E2 by (metis old.prod.exhaust) 
+    using relpowp_Suc_E2 by auto
     then have "(c1';;c2, s1') \<rightarrow>* (t1 + t2 + 1) \<down> (c3, s3)" using Suc by blast 
-    then show ?case using Suc by (metis Seq2 * add_Suc relpowp_Suc_I2)
+    then show ?case using Suc by auto
   qed
 
-text "transitive closure property"
-lemma sum_transitive: "\<lbrakk>cc \<rightarrow>* x \<down> cc'; cc' \<rightarrow>* y \<down> cc''\<rbrakk> \<Longrightarrow> cc \<rightarrow>* x + y \<down> cc'' "
-  by (simp add: relcomppI relpowp_add)
 
 text "from Big step to small step semantics"
 lemma big_to_small_helper: "(c, s) \<Rightarrow> t \<Down> s' \<Longrightarrow> t = Suc t' \<Longrightarrow> (c, s) \<rightarrow>* t' \<down> (SKIP, s')"
@@ -119,7 +113,7 @@ next
   case (Assign x a s)
   then have "t' = Suc 0" by auto
   moreover have "(x ::= a, s) \<rightarrow> (SKIP, s(x := aval a s))" by blast
-  ultimately show ?case by (meson relpowp_0_I relpowp_Suc_I)
+  ultimately show ?case by blast
 next
   case (Seq c1 s1 x s2 c2 y s3 z)
   then obtain x' y' where suc_ex: "x = Suc x'" "y = Suc y'"
@@ -130,16 +124,12 @@ next
   then show ?case using Seq suc_ex by simp
 next
   case (IfTrue s b c1 x t y c2)
-  then obtain x' where "x = Suc x'" by (meson bigstep_progress gr0_implies_Suc)
-  then show ?case using IfTrue 
-    by (metis add_diff_cancel_left' add_diff_cancel_right'
-        plus_1_eq_Suc relpowp_Suc_I2 small_step.IfTrue)
+  then obtain x' where "x = Suc x'" using bigstep_progress gr0_implies_Suc by blast
+  then show ?case using IfTrue by auto
 next
   case (IfFalse s b c2 x t y c1)
   then obtain x' where "x = Suc x'" by (meson bigstep_progress gr0_implies_Suc)
-  then show ?case using IfFalse
-    by (metis add_diff_cancel_left' add_diff_cancel_right'
-        plus_1_eq_Suc relpowp_Suc_I2 small_step.IfFalse)
+  then show ?case using IfFalse by auto
 next
   case (WhileFalse s b c)
   then show ?case by fastforce
@@ -153,7 +143,7 @@ next
     using WhileTrue by (simp add: suc_def star_seq2) 
   moreover have "(SKIP ;; ?w, s2) \<rightarrow>* 1 \<down>(?w, s2)" by auto
   moreover have "(?w, s2) \<rightarrow>* y' \<down> (SKIP, s3)" using WhileTrue  \<open>y = Suc y'\<close> by blast
-  ultimately have "(?w, s1) \<rightarrow>* 1 + x' + 1 + y'\<down> (SKIP, s3)" using sum_transitive by blast 
+  ultimately have "(?w, s1) \<rightarrow>* 1 + x' + 1 + y'\<down> (SKIP, s3)" by (meson rel_pow_sum) 
   moreover have "t' = Suc (Suc (x' + y')) " using WhileTrue suc_def by auto 
   thus ?case  using calculation by simp
 qed
@@ -173,8 +163,8 @@ lemma small1_big_continue:
 lemma small_to_big :
  "cs \<rightarrow>* t \<down> (SKIP,s') \<Longrightarrow> cs \<Rightarrow> Suc t \<Down> s' "
   apply(induction t arbitrary:cs s')
-  apply auto[1]
-  by (metis relpowp_Suc_E2 small1_big_continue)
+   apply auto[1]
+  using small1_big_continue by blast
 
 text "Equivalence statement"
 lemma equiv_small_big_pair:
