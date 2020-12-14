f1ba24b0fa987bea4fb41a5193136710d26b58a7
Bilel Ghorbel <ge36toh@mytum.de>
2020-12-14 17:10:41 +0100

    cook-levin

--- Complexity_classes_def/P.thy
+++ Complexity_classes_def/P.thy
@@ -1,4 +1,4 @@
-theory P imports "../IMP-/Big_StepT"  "../Polynomial_Growth_Functions" begin
+theory P imports "../IMP-/Big_StepT"  "../Polynomial_Growth_Functions"  "../Three_Sat_To_Set_Cover"begin
 
 type_synonym lang = "nat set"
 type_synonym bound_fun = "nat \<Rightarrow> nat"
@@ -276,6 +276,51 @@ proof
   qed
 qed
 
+lemma result_bounded_compose:
+  assumes "result_bounded f pf" "result_bounded g pg"
+  shows "result_bounded (f;;g) (make_mono pg o pf)"
+  apply(auto simp add:result_bounded_def)
+proof -
+  fix x 
+  obtain y z
+   where yz_def:"comp f x y" "comp g y z" "length y \<le> pf (length x)" "length z \<le> pg(length y)"
+   using assms result_bounded_def by blast
+  hence  "comp (f;;g) x z" using comp_comp by simp
+  moreover have  "length z \<le> make_mono pg (length y)" using yz_def(4) le_trans by blast 
+  hence  " length z \<le> make_mono pg (pf (length x))"
+    using yz_def mono_make_mono  le_trans monoD by blast
+ ultimately show "\<exists>r. P.comp (f;; g) x r \<and> P.length r \<le> make_mono pg (pf (P.length x))" by auto
+qed
+
+lemma poly_result_compose : 
+  assumes "poly_result_bounded f" "poly_result_bounded g"
+  shows "poly_result_bounded (f;;g)"
+   proof -
+    obtain pf pg where pf_pg_def: "poly pf" "poly pg " "result_bounded f pf" "result_bounded g pg" 
+      using assms is_polyreduction_def poly_result_bounded_def by auto
+    have "poly (make_mono pg o pf)" 
+      by (simp add: pf_pg_def(1) pf_pg_def(2) poly_compose poly_make_mono_iff)
+    moreover have "result_bounded (f;;g) (make_mono pg o pf)"
+      using result_bounded_compose pf_pg_def by auto
+    ultimately show ?thesis by (auto simp add:poly_result_bounded_def)
+  qed
+
+lemma is_reduction_compose:
+  assumes "is_reduction f D D'" "is_reduction g D' D''"
+  shows "is_reduction (f;;g) D D''"
+  apply(auto simp add: is_reduction_def)
+  using assms apply(simp add: cons_certif_def is_reduction_def) apply fastforce
+proof -
+  fix x
+   obtain y where y_def: "comp f x y" "(x\<in>D) =(y\<in>D')"
+     using assms(1) by (auto simp add:is_reduction_def)
+   obtain z where z_def: "comp g y z" "(y\<in>D') =(z\<in>D'')"
+     using assms(2) by (auto simp add:is_reduction_def)    
+   have  "P.comp (f;; g) x z  \<and> (x \<in> D) = (z \<in> D'')"
+     using y_def z_def comp_comp by simp
+   thus  "\<exists>r. P.comp (f;; g) x r \<and> (x \<in> D) = (r \<in> D'')" by blast
+ qed
+
 lemma "poly_reduces_trans":
   assumes "poly_reduces D D'"  "poly_reduces D' D''" 
   shows "poly_reduces D D''"
@@ -286,42 +331,9 @@ proof -
   have  "is_polyreduction ?c D D''" 
     apply (auto simp add:is_polyreduction_def  reduction_poly)
     using fg_def apply (simp add: is_polyreduction_def reduction_poly)
-    apply(auto simp add:is_reduction_def)
-    using fg_def apply (simp add:cons_certif_def is_polyreduction_def is_reduction_def) 
-      apply fastforce 
-    subgoal for x
-    proof - 
-      obtain y z where "comp f x y" "comp g y z" "(x\<in>D) =(y\<in>D')" "(y\<in>D') = (z \<in> D'')" 
-        using fg_def is_polyreduction_def poly_result_bounded_def result_bounded_def
-        by (metis (full_types) is_reduction_def) 
-      hence  "comp (f;; g) x z \<and> (x \<in> D) = (z \<in> D'')" using comp_comp by blast
-      thus ?thesis by blast
-    qed
-    apply(auto simp add:poly_result_bounded_def)
-  proof -
-    obtain pf pg where pf_pg_def: "poly pf" "poly pg " "result_bounded f pf" "result_bounded g pg" 
-      using fg_def(1) fg_def(2) is_polyreduction_def poly_result_bounded_def by auto
-    have "poly (make_mono pg o pf) \<and> result_bounded (f;;g) (make_mono pg o pf)" 
-      apply auto
-      apply (simp add: pf_pg_def(1) pf_pg_def(2) poly_compose poly_make_mono_iff)
-      apply(simp add:result_bounded_def)
-      subgoal
-      proof
-        fix x
-        obtain y z
-          where yz_def:"comp f x y" "comp g y z" "length y \<le> pf (length x)" "length z \<le> pg(length y)"
-          using pf_pg_def(3) pf_pg_def(4) result_bounded_def by blast
-        have "length z \<le> make_mono pg (length y)" using yz_def(4) le_trans by blast 
-        hence  " length z \<le> make_mono pg (pf (length x))"
-          using yz_def mono_make_mono  le_trans monoD by blast
-        moreover have "comp (f;;g) x z" 
-          using yz_def comp_comp by blast
-        ultimately show "\<exists>r. P.comp (f;; g) x r \<and> P.length r \<le> make_mono pg (pf (P.length x))" 
-          by auto
-      qed
-      done
-      then show  "\<exists>p. poly p \<and> result_bounded (f;; g) p " by blast
-  qed
+    using fg_def is_reduction_compose apply (simp add: is_polyreduction_def) apply blast
+    using fg_def apply (auto simp add: poly_result_compose is_polyreduction_def)
+    done
   thus "poly_reduces D D''" by (auto simp add:poly_reduces_def)
 qed
 lemma "poly_time_bounded c \<Longrightarrow> poly_result_bounded c" oops
@@ -331,4 +343,140 @@ definition NP_hard :: "lang set" where
 
 definition NP_complete :: "lang set" where
 "NP_complete \<equiv> NP_hard \<inter> NP"
-  
+
+locale encode_decode_sat =
+  fixes encode_sat :: "nat three_sat \<Rightarrow> nat"
+  fixes decode_sat :: "nat\<Rightarrow> nat three_sat"
+  assumes decode_encode_inv : "decode_sat (encode_sat F) = F"
+begin
+definition IMP_SAT :: "nat set" where 
+"IMP_SAT == encode_sat ` {n. sat n}"
+
+lemma main_lemma : 
+  fixes c p
+  assumes "poly p"
+  assumes "\<forall>s. \<exists>t. Ex (big_step_t (c, s) t) \<and> t \<le> p (length (s ''input''))"
+  assumes " \<forall>x z. \<exists>r. \<forall>s. s ''input'' = x \<and> s ''certificate'' = z \<longrightarrow>
+                         (\<exists>t s'. (c, s) \<Rightarrow>\<^bsup> t \<^esup> s' \<and> s' ''input'' = r)"
+  shows "\<exists>imp_to_sat t_red s_red.
+         poly t_red 
+       \<and> poly s_red
+       \<and> (\<forall>s t s'. (imp_to_sat,s) \<Rightarrow>\<^bsup> t \<^esup> s' \<longrightarrow> s ''certificate'' = s' ''certificate'')
+       \<and> (\<forall>x. \<exists>f.    length f \<le> s_red ( length x )
+                   \<and> (\<forall>s. s ''input'' = x \<longrightarrow> (\<exists>t s'. (imp_to_sat,s) \<Rightarrow>\<^bsup> t \<^esup> s' \<and> 
+                                          s' ''input'' = f \<and> t \<le> t_red(length x)))
+                   \<and> ( f \<in> IMP_SAT \<longleftrightarrow>
+                                        (\<exists>z. \<forall>s t s'. s ''input'' = x 
+                                                      \<and> s ''certificate'' = z 
+                                                      \<and>(v, s) \<Rightarrow>\<^bsup> t \<^esup> s' \<longrightarrow> s' ''input'' >0)
+                                        )
+                      )"
+  sorry
+lemma NP_reduces_SAT:
+  assumes "L \<in> NP"
+  shows "poly_reduces L IMP_SAT"
+proof -
+  obtain v p where pv_def:"poly p" "is_verif v L" "time_bounded v p" using assms
+    by (auto simp add: NP_def is_poly_verif_def poly_time_bounded_def)
+  have "poly p" using pv_def by simp
+  moreover have "\<forall>s. \<exists>t. Ex (big_step_t (v, s) t) \<and> t \<le> p (length (s ''input''))" using pv_def(3) 
+    by(auto simp add: time_bounded_def)
+  moreover have " \<forall>x z. \<exists>r. \<forall>s. s ''input'' = x \<and> s ''certificate'' = z \<longrightarrow>
+                         (\<exists>t s'. (v, s) \<Rightarrow>\<^bsup> t \<^esup> s' \<and> s' ''input'' = r)" using pv_def (2)
+    by (auto simp add: is_verif_def verif_def)
+  ultimately have "\<exists>imp_to_sat t_red s_red. 
+      poly t_red \<and> 
+      poly s_red \<and>
+       (\<forall>s t s'. (imp_to_sat,s) \<Rightarrow>\<^bsup> t \<^esup> s' \<longrightarrow> s ''certificate'' = s' ''certificate'') \<and>
+      (\<forall>x. \<exists>f. length f \<le> s_red ( length x )
+              \<and> (\<forall>s. s ''input'' = x \<longrightarrow> (\<exists>t s'. (imp_to_sat,s) \<Rightarrow>\<^bsup> t \<^esup> s' \<and> 
+                                          s' ''input'' = f \<and> t \<le> t_red(length x)))
+              \<and> ( f \<in> IMP_SAT \<longleftrightarrow>
+                    (\<exists>z. \<forall>st t st'. st ''input'' = x \<and> st ''certificate'' = z \<and>(v, st) \<Rightarrow>\<^bsup> t \<^esup> st'
+                                         \<longrightarrow> st' ''input'' >0)
+                                       )
+      )" using main_lemma by auto
+  then obtain imp_to_sat t_red s_red where main_res:
+          "poly t_red " "poly s_red" "\<forall>x. \<exists>f. (length f \<le> s_red ( length x ))
+              \<and> (\<forall>s. s ''input'' = x \<longrightarrow> (\<exists>t s'. (imp_to_sat,s) \<Rightarrow>\<^bsup> t \<^esup> s' \<and> 
+                                          s' ''input'' = f \<and> t \<le> t_red(length x)))
+              \<and> ( f \<in> IMP_SAT \<longleftrightarrow>
+                    (\<exists>z. \<forall>s t s'. s ''input'' = x \<and> s ''certificate'' = z 
+                             \<and> (v, s) \<Rightarrow>\<^bsup> t \<^esup> s'
+                                         \<longrightarrow> s' ''input'' >0)
+                                       )"
+ "cons_certif imp_to_sat"  by (auto simp add:cons_certif_def)
+  have "time_bounded imp_to_sat t_red"  using main_res(3)  by (auto simp add:time_bounded_def) blast 
+  hence "poly_time_bounded imp_to_sat" using main_res(1) by (auto simp add:poly_time_bounded_def)
+  moreover have "result_bounded imp_to_sat s_red"
+    apply (auto simp add: result_bounded_def comp_def) 
+     using main_res(3) by metis
+  hence "poly_result_bounded imp_to_sat" using main_res(2) by(auto simp add:poly_result_bounded_def)
+  moreover have "is_reduction imp_to_sat L IMP_SAT" apply (auto simp add:is_reduction_def)
+    using main_res(4) apply simp
+    subgoal for x
+  proof -
+    have  "\<exists>f. (length f \<le> s_red ( length x ))
+              \<and> (\<forall>s. s ''input'' = x \<longrightarrow> (\<exists>t s'. (imp_to_sat,s) \<Rightarrow>\<^bsup> t \<^esup> s' \<and> 
+                                          s' ''input'' = f \<and> t \<le> t_red(length x)))
+              \<and> ( f \<in> IMP_SAT \<longleftrightarrow>
+                    (\<exists>z. \<forall>s t s'. s ''input'' = x \<and> s ''certificate'' = z 
+                             \<and> (v, s) \<Rightarrow>\<^bsup> t \<^esup> s'
+                                         \<longrightarrow> s' ''input'' >0)
+                                       )"
+      using main_res(3) 
+      by simp
+    then obtain f where f_def: "\<forall>s. s ''input'' = x \<longrightarrow> (\<exists>t s'. (imp_to_sat,s) \<Rightarrow>\<^bsup> t \<^esup> s' \<and> 
+                                          s' ''input'' = f \<and> t \<le> t_red(length x))" 
+                        " f \<in> IMP_SAT \<longleftrightarrow>
+                    (\<exists>z. \<forall>s t s'. s ''input'' = x \<and> s ''certificate'' = z 
+                             \<and> (v, s) \<Rightarrow>\<^bsup> t \<^esup> s'
+                                         \<longrightarrow> s' ''input'' >0)"
+      by auto
+    have "comp imp_to_sat x f" using f_def(1) apply (simp add: comp_def) by blast
+    moreover have "x\<in>L \<longleftrightarrow> f \<in> IMP_SAT"
+    proof
+      assume "x\<in>L"
+      hence  "\<exists>z r. verif v x z r \<and> 0 < r" using pv_def by (auto simp add: is_verif_def)
+      then obtain z  where z_def: "\<forall>r. verif v x z r \<longrightarrow>0< r " using verif_det by blast
+      have "\<forall>s t s'. s ''input'' = x \<and> s ''certificate'' = z 
+                             \<and> (v, s) \<Rightarrow>\<^bsup> t \<^esup> s'
+                                         \<longrightarrow> s' ''input'' >0"
+        apply auto
+        subgoal for s t s' 
+        proof -
+          assume assms: " x = s ''input'' " "(v, s) \<Rightarrow>\<^bsup> t \<^esup> s'"  "z = s ''certificate''"
+          obtain r where r_def :"verif v x z r" using pv_def(2) apply(auto simp add: is_verif_def) by blast
+          hence "r = s' ''input'' " apply(simp add: verif_def)
+            using assms big_step_t_determ2 by blast 
+          hence "verif v x z (s' ''input'')" using r_def by simp 
+          thus " 0 < s' ''input'' " using z_def  by simp
+        qed
+        done
+      thus "f \<in> IMP_SAT" using f_def(2) bigstep_det  by (auto simp add: verif_def) 
+    next 
+      assume  "f \<in> IMP_SAT"
+      then obtain z where z_def: "\<forall>s t s'. s ''input'' = x \<and> s ''certificate'' = z  \<and>(v, s) \<Rightarrow>\<^bsup> t \<^esup> s'
+                           \<longrightarrow> s' ''input'' >0" using f_def by blast 
+      obtain r where r_def :"verif v x z r" using pv_def(2) apply(auto simp add: is_verif_def)
+        by blast
+      obtain s where "s = <''input'':=x, ''certificate'':=z>" by simp
+      then have s_def: " s ''input'' = x \<and> s ''certificate'' = z" by simp
+      obtain t s' where ts'_def: " (v, s) \<Rightarrow>\<^bsup> t \<^esup> s'" "s' ''input'' = r"
+        using r_def s_def apply(simp add: verif_def) by blast
+      
+      have "s' ''input'' > 0" using s_def ts'_def(1) z_def by auto
+      hence "r>0" using ts'_def(2) by simp
+      thus   "x \<in> L" using pv_def(2) using is_verif_def r_def by blast
+    qed
+    ultimately show ?thesis  by auto
+  qed
+  done
+ultimately show ?thesis by (auto simp add:poly_reduces_def is_polyreduction_def)
+qed
+
+lemma cook_levin: "IMP_SAT \<in> NP_hard"
+  by (simp add: NP_hard_def NP_reduces_SAT)
+end
+
+
