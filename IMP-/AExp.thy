b31710b8b07debc9a237a8cc5dd9e951fd67d00e
Bilel Ghorbel <ge36toh@mytum.de>
2021-03-12 10:41:51 +0100

    syntactic dugar

--- IMP-/AExp.thy
+++ IMP-/AExp.thy
@@ -15,7 +15,6 @@ type_synonym state = "vname \<Rightarrow> val"
 text "Defining atomic expressions:"
 datatype atomExp = N val | V vname
 
-
 fun atomVal :: "atomExp \<Rightarrow> state \<Rightarrow> val" where
 "atomVal (V var) s = s var"|
 "atomVal (N number) _ = number"
@@ -27,17 +26,33 @@ datatype aexp =  A atomExp            |
                  Parity atomExp       |
                  RightShift atomExp
 
+bundle aexp_syntax
+begin
+notation Plus            ("_ \<oplus> _" [60,60] 60) and
+         Sub             ("_ \<ominus> _" [60,60] 60) and
+         Parity          ("_ \<doteq>1" [60] 60)   and
+         RightShift      ("_\<then>" [60] 60)
+
+end
+bundle no_aexp_syntax
+begin
+notation Plus            ("_ \<oplus> _" [60,60] 60) and
+         Sub             ("_ \<ominus> _" [60,60] 60) and
+         Parity          ("_ \<doteq>1" [60] 60)    and
+         RightShift      ("_\<then>" [60] 60)
+end
+unbundle aexp_syntax
 
 fun aval :: "aexp \<Rightarrow> state \<Rightarrow> val" where
 "aval (A atomExp) s = atomVal atomExp s"        |
-"aval (Plus a b) s = atomVal a s  + atomVal b s"|
-"aval (Sub a b) s = atomVal a s - atomVal b s"  |
-"aval (Parity a) s = atomVal a s  mod 2"        |
-"aval (RightShift a) s = atomVal a s div 2"
+"aval (a \<oplus> b) s = atomVal a s  + atomVal b s" |
+"aval (a \<ominus> b) s = atomVal a s - atomVal b s"  |
+"aval (a \<doteq>1) s = atomVal a s  mod 2"        |
+"aval (a \<then>) s = atomVal a s div 2"
 
 text "evaluation examples:"
-value "aval (Plus (V ''x'') (N 5)) (\<lambda>x. if x = ''x'' then 7 else 0)"
-value "aval (Plus (V ''x'') (N 5)) ((\<lambda>x. 0) (''x'':= 7))"
+value "aval (V ''x'' \<oplus> N 5)  (\<lambda>x. if x = ''x'' then 7 else 0)"
+value "aval (V ''x'' \<ominus> N 5)  ((\<lambda>x. 0) (''x'':= 7))"
 
 text "Syntactic sugar to write states:"     
 definition null_state ("<>") where
@@ -61,7 +76,7 @@ end
 lemma "<a := 1, b := 2> = (<> (a := 1)) (b := (2::int))"
   by (rule refl)
 
-value "aval (Sub (V ''x'') (N 5)) <''x'' := 7>"
-value "aval (Sub (V ''x'') (N 10)) <''x'' := 7>"
+value "aval (V ''x'' \<ominus> N 5) <''x'' := 7>"
+value "aval (V ''x'' \<ominus> N 10) <''x'' := 7>"
 
 end
