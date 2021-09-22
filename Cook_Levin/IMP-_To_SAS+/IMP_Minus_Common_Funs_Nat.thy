b742b3534c39f0bf7c8eb897604c47bad82fe536
Taro Yoshioka <t.yoshioka@tum.de>
2021-09-22 03:56:34 +0200

    stopped working on append

--- Cook_Levin/IMP-_To_SAS+/IMP_Minus_Common_Funs_Nat.thy
+++ Cook_Levin/IMP-_To_SAS+/IMP_Minus_Common_Funs_Nat.thy
@@ -2,4 +2,25 @@ theory IMP_Minus_Common_Funs_Nat
   imports "../../IMP-/IMP_Minus_Nat_Bijection"
 begin
 
+subsection \<open>append\<close>
+
+text\<open>Goal of this subsection is *append_tail*, which is defined in terms
+  of reverse_nat and append_acc, both of which, in turn, are defined in
+  terms of -- or can be related to -- *reverse_nat_acc*.\<close>
+
+lemma "append_acc acc xs = reverse_nat_acc acc xs"
+  by(induction xs arbitrary: acc rule: append_acc.induct) simp+
+
+(*
+  append_tail xs ys
+= reverse_nat       (append_acc      (reverse_nat       xs) ys)
+= reverse_nat       (reverse_nat_acc (reverse_nat       xs) ys)
+= reverse_nat_acc 0 (reverse_nat_acc (reverse_nat_acc 0 xs) ys)
+*)
+
+(*
+-- stop -- usage of function --
+*)
+
+
 end
