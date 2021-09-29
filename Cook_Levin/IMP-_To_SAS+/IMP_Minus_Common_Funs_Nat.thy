d5204ab60ed930eb628b0ffcd142952147a9022d
lakiryt <lakiryt@icloud.com>
2021-09-29 20:52:37 +0200

    shortened proof for elemof_IMP_Minus_iteration_correct

--- Cook_Levin/IMP-_To_SAS+/IMP_Minus_Common_Funs_Nat.thy
+++ Cook_Levin/IMP-_To_SAS+/IMP_Minus_Common_Funs_Nat.thy
@@ -75,81 +75,24 @@ lemma elemof_IMP_Minus_iteration_correct:
     ''snd_nat'' := 0)"
 proof(cases "hd_nat (s ''f'') = s ''e''")
   case True
-  then have "fst_nat (s ''f'' - Suc 0) - s ''e'' +
-        (s ''e'' -
-         fst_nat (s ''f'' - Suc 0)) = 0" 
-    using hd_nat_def by auto
   then show ?thesis
     unfolding elemof_IMP_Minus_iteration_def
       elemof_IMP_Minus_iteration_time_def
-    by (fastforce simp: True
-        intro: terminates_in_time_state_intro[OF Big_StepT.Assign]
-        terminates_in_time_state_intro[OF Big_StepT.IfFalse]
-        terminates_in_time_state_intro[OF zero_variables_correct]
-        terminates_in_time_state_intro[OF IMP_Minus_fst_nat_correct]
+    by (fastforce simp: hd_nat_def
+        intro!: terminates_in_time_state_intro[OF Seq']
+        intro:zero_variables_correct IMP_Minus_fst_nat_correct
         )+
 next
   case False
-  then have 1: "fst_nat (s ''f'' - Suc 0) - s ''e'' +
-        (s ''e'' -
-         fst_nat (s ''f'' - Suc 0)) \<noteq> 0" 
-    using hd_nat_def by auto
-  then have 0: "(s(''c'' := 0, ''fst_nat'' := fst_nat (s ''f'' - Suc 0), ''b'' := s ''e'' - fst_nat (s ''f'' - Suc 0),
-   ''a'' := fst_nat (s ''f'' - Suc 0) - s ''e'' + (s ''e'' - fst_nat (s ''f'' - Suc 0))))
- ''a'' \<noteq> 0" by simp
-
-  have 2: "( ''a'' ::= (V ''f'' \<ominus> N 1);;
-          IMP_Minus_snd_nat;;
-          ''f'' ::= A (V ''snd_nat''),
-    s(''c'' := 0,
-      ''fst_nat'' := fst_nat (s ''f'' - Suc 0),
-      ''b'' := s ''e'' - fst_nat (s ''f'' - Suc 0),
-      ''a'' := fst_nat (s ''f'' - Suc 0) - s ''e'' +
-              (s ''e'' - fst_nat (s ''f'' - Suc 0))
-      )    )
-     \<Rightarrow>\<^bsup> 2 + IMP_Minus_fst_nat_time (s ''f'' - 1) + 2 \<^esup>
-      s(''a'' := 0, ''b'' := 0, ''c'' := 0,
-        ''f'' := snd_nat (s ''f'' - Suc 0),
-        ''fst_nat'' := fst_nat (s ''f'' - Suc 0),
-        ''snd_nat'' := snd_nat (s ''f'' - 1))"
-    by (auto simp: hd_nat_def
-        intro!: terminates_in_time_state_intro[OF Seq']
-      intro: IMP_Minus_snd_nat_correct)
-
-  have 3: "2 + IMP_Minus_fst_nat_time (s ''f'' - 1) + 2 + 1 =
-  (if s ''e'' = hd_nat (s ''f'') then 2 + 2 + 1
-    else 2 + IMP_Minus_fst_nat_time (s ''f'' - 1) + 2 + 1
-  )"
-    using False by simp
-
-  show ?thesis
+  then show ?thesis
     unfolding elemof_IMP_Minus_iteration_def
-        elemof_IMP_Minus_iteration_time_def
-    apply(rule Seq')+
-    apply(fastforce
-        intro: terminates_in_time_state_intro[OF Big_StepT.Assign]
-        )
-    apply(fastforce
-        intro: terminates_in_time_state_intro[OF IMP_Minus_fst_nat_correct]
-        )
-    apply(fastforce
-        intro: terminates_in_time_state_intro[OF Big_StepT.Assign]
+      elemof_IMP_Minus_iteration_time_def
+    by (fastforce simp: hd_nat_def tl_nat_def 
+        intro!: terminates_in_time_state_intro[OF Seq']
+        intro: zero_variables_correct
+        IMP_Minus_fst_nat_correct
+        IMP_Minus_snd_nat_correct
         )+
-     apply(fastforce simp add: False 1
-        intro!: terminates_in_time_state_intro[OF Big_StepT.IfTrue ,
-        of "s
-   (''c'' := 0, ''fst_nat'' := fst_nat (s ''f'' - Suc 0), ''b'' := s ''e'' - fst_nat (s ''f'' - Suc 0),
-    ''a'' := fst_nat (s ''f'' - Suc 0) - s ''e'' + (s ''e'' - fst_nat (s ''f'' - Suc 0)))"
-        "''a''"
-        "''a'' ::= (V ''f'' \<ominus> N 1);; IMP_Minus_snd_nat;; ''f'' ::= A (V ''snd_nat'')"
-      , OF 0 2 3[symmetric]
-        ])
-    using False
-    apply simp
-    apply(fastforce simp add: tl_nat_def
-        intro: terminates_in_time_state_intro[OF zero_variables_correct]
-        )
-    done
 qed
 
 
